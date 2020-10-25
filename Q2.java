0827ac1b8e2b5c32ac0053da68e14d9a76086231
yyildizz <64442783+yyildizz@users.noreply.github.com>
2020-10-25 15:25:06 +0300

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -14,12 +14,8 @@ public class Q2 {
 	}
 
 	public static void love6(int num1, int num2) {
-		if (num1 == 6 || num2 == 6)
-			System.out.println(true);
-		else if (num1 + num2 == 6)
-			System.out.println(true);
-		else
-			System.out.println(false);
+		System.out.println(num1 == 6 || num2 == 6 || num1 + num2 == 6 || num1 - num2 == 6 || num2 - num1 == 6);
+
 	}
 
 }
