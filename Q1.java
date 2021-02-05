0621092469bccd08d690308a77c397952a73c789
Yalcin YILDIZ <64442783+yyildizz@users.noreply.github.com>
2021-02-05 07:02:17 +0200

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -5,7 +5,9 @@ public class Q1 {
 	public static void main(String[] args) {
 //		Write a method that accepts a string and a non-negative number, and prints a larger string that is number copies of the original String
 
-		stringTimes("Hi", 2);
+//	stringTimes("Hi", 2);
+		String msg ="mesaj";
+		System.out.println(msg.substring(4,4));;
 	}
 
 	public static void stringTimes(String message, int count) {
